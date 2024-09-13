c5abe945223b87ad0022cb75b5222fc043cde758
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-09-13 23:27:32 +0900

    [upd] prj3

--- prj3/main.c
+++ prj3/main.c
@@ -8,10 +8,19 @@ static int func(int n)
     return sizeof(x);
 }
 
+static int func2(int n, int m)
+{
+    int x[n][m];
+    return sizeof(x);
+}
+
 int main()
 {
     int ret = func(10);
     printf("%lu %d\n", sizeof(int), ret);
+
+    int ret2 = func2(3, 4);
+    printf("%lu %d\n", sizeof(int), ret2);
 
     return 0;
 }

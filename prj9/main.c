cf7e7b129e980405a05c32744b63dc1e9598ab2b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-09-27 14:55:53 +0900

    [upd] prj9

--- prj9/main.c
+++ prj9/main.c
@@ -9,33 +9,34 @@
 
 typedef struct object_holder {
   void **objects;
-  size_t size;
-  void (*processor)(void *);
+  size_t n_objects;
 } object_holder;
 
-void run_and_free(object_holder *holder) {
-  holder->processor(holder);
+typedef void (*object_processor)(void *);
+void run_and_free(void *holder_, object_processor processor) {
+  object_holder *holder = (object_holder *)holder_;
+  processor(holder);
 
-  for (size_t i = 0; i < holder->size; i++) {
+  printf("releasing objects\n");
+  for (size_t i = 0; i < holder->n_objects; i++) {
     free(holder->objects[i]);
   }
 }
 
 typedef struct {
   void **objects;
-  size_t size;
-  void (*processor)(void *);
+  size_t n_objects;
   size_t n;
   double *sumproduct;
 } sumproduct_context;
 
-void sumproduct_processor(void *context) {
-  sumproduct_context *ctx = (sumproduct_context *)context;
-  double *x = (double *)ctx->objects[0];
-  double *y = (double *)ctx->objects[1];
-  *ctx->sumproduct = 0;
-  for (size_t i = 0; i < ctx->n; i++) {
-    *ctx->sumproduct += x[i] * y[i];
+void sumproduct_processor(void *self_) {
+  sumproduct_context *self = (sumproduct_context *)self_;
+  double *x = (double *)self->objects[0];
+  double *y = (double *)self->objects[1];
+  *self->sumproduct = 0;
+  for (size_t i = 0; i < self->n; i++) {
+    *self->sumproduct += x[i] * y[i];
   }
 }
 
@@ -49,8 +50,8 @@ int main() {
   }
   void *objects[2] = {x, y};
   double sumproduct = 0;
-  sumproduct_context ctx = {objects, 2, sumproduct_processor, n, &sumproduct};
-  sumproduct_processor(&ctx);
+  sumproduct_context ctx = {objects, 2, n, &sumproduct};
+  run_and_free(&ctx, sumproduct_processor);
   printf("%f\n", sumproduct);
 
   return 0;
